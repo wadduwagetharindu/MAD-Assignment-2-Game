3ab39a2c690cf412e467e0a280fa5e4e894886c6
wadduwagetharindu <wadduwagetharindu2@gmail.com>
2024-02-15 10:58:57 +0530

    flutter_application_3_game4
    Add flutter_application_3_game4 File

---
+++ flutter_application_3_game4/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_application_3_game4/screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
